e89c1ddfe7cc218ada7ad7cbaa7b19a59325be9b
yhy0009 <ref0009@naver.com>
2025-05-12 15:54:47 +0900

    feat add delete method

--- src/main/java/app/api/controller/EmpAPIController.java
+++ src/main/java/app/api/controller/EmpAPIController.java
@@ -1,14 +1,20 @@
 package app.api.controller;
 
-import app.repository.EmpRepository;
+import app.entity.Emp;
+import app.service.EmpService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
+import org.springframework.web.bind.annotation.*;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/emp")
 public class EmpAPIController {
 
-    private final EmpRepository empRepository;
+    private final EmpService empService;
 
-
+    @DeleteMapping("/{empno}")
+    public Emp deleteEmp(@PathVariable Integer empno) {
+        return empService.deleteEmp(empno);
+    }
 }
