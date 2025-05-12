5acde5fe3b42d245a3b8194de96c7ad8f46ec234
siiiirru <eqeee@naver.com>
2025-05-12 16:02:03 +0900

    feat:emp 조회 기능 추가

--- src/main/java/app/api/controller/EmpAPIController.java
+++ src/main/java/app/api/controller/EmpAPIController.java
@@ -1,14 +1,25 @@
 package app.api.controller;
 
+import app.entity.Dept;
+import app.entity.Emp;
 import app.repository.EmpRepository;
+import app.service.EmpService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class EmpAPIController {
 
-    private final EmpRepository empRepository;
-
-
+    private final EmpService empService;
+    
+	@GetMapping("/api/emp/{empno}")
+	public Emp getDeptByDeptno(@PathVariable Integer empno) {
+		Emp emp = empService.getEmpByEmpno(empno);
+		return emp;
+	}
 }
